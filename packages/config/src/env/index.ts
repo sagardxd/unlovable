35387cd52e8cb62d188378c1e708e76fc00938f9
sagardxd <sagardxd5@gmail.com>
2025-11-02 06:49:55 +0530

    feat: update environment schema to include additional configuration variables

--- packages/config/src/env/index.ts
+++ packages/config/src/env/index.ts
@@ -7,7 +7,11 @@ dotenv.config({path: path.resolve(__dirname, '../../../../.env')})
 const envScehma = z.object({
     BACKEND_PORT: z.string().transform(Number),
     JWT_TOKEN_PASS: z.string(),
-    HASH_SALT: z.string()
+    HASH_SALT: z.string(),
+    OPENROUTER_API_KEY: z.string(),
+    E2B_API_KEY: z.string(),
+    TEMPLATE_ID: z.string(),
+    REDIS_URL: z.string()
 })
 
 const env = envScehma.parse(process.env)
@@ -15,5 +19,9 @@ const env = envScehma.parse(process.env)
 export const config = {
     BACKEND_PORT: env.BACKEND_PORT,
     JWT_TOKEN_PASS: env.JWT_TOKEN_PASS,
-    HASH_SALT: env.HASH_SALT
+    HASH_SALT: env.HASH_SALT,
+    OPENROUTER_API_KEY: env.OPENROUTER_API_KEY,
+    E2B_API_KEY: env.E2B_API_KEY,
+    TEMPLATE_ID: env.TEMPLATE_ID,
+    REDIS_URL: env.REDIS_URL
 }
